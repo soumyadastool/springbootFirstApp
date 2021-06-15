383c93c22269e3eb8f9308bbcb2259960a706131
tl@gmail.com <Dell@Dell-PC>
2021-06-15 06:33:14 -0400

    First Time File Update

---
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome To KdotsDigital");
+
+	}
+
+}
